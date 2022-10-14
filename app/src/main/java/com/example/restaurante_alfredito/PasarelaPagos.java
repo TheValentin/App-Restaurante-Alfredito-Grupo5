eb1ad247f812a5d34c44f03b9ae55077f5151212
Raven2491 <er.canales.24@gmail.com>
2022-10-14 18:19:47 -0500

    Pasarela de pagos

---
+++ app/src/main/java/com/example/restaurante_alfredito/PasarelaPagos.java
@@ -0,0 +1,14 @@
+package com.example.restaurante_alfredito;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PasarelaPagos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pasarela_pagos);
+    }
+}
