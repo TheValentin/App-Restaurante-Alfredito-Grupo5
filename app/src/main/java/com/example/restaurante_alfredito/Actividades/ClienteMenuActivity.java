df110aaf6e4d148cb632b2ce1d1683ededf07e49
Raven2491 <er.canales.24@gmail.com>
2022-10-14 20:06:09 -0500

    Pasareladepagos2

--- app/src/main/java/com/example/restaurante_alfredito/Actividades/ClienteMenuActivity.java
+++ app/src/main/java/com/example/restaurante_alfredito/Actividades/ClienteMenuActivity.java
@@ -1,10 +1,13 @@
 package com.example.restaurante_alfredito.Actividades;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
 
 
+import com.example.restaurante_alfredito.PasarelaPagos;
 import com.example.restaurante_alfredito.R;
 import com.example.restaurante_alfredito.databinding.ActivityClienteMenuBinding;
 import com.google.android.material.snackbar.Snackbar;
